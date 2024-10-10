9f4c268105a467c303db1647161b0c2c49888723
divyanshu-vashu <vashusingh2004.jan@gmail.com>
2024-10-10 22:17:58 +0530

    added env support in port

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const express=require('express')
-const app=express()
+const express = require('express');
+const app = express();
 const cors = require("cors");
 const connection = require("./db");
 
-const port=5000
+// Use environment variable for port or default to 5000
+const port = process.env.PORT || 5000;
+
 const userRoutes = require("./routes/aliceAuth");
-const aliceDetails=require("./routes/aliceDetails");
-const addTrans= require("./routes/addTrans");
-const allDetails=require("./routes/allTrans")
+const aliceDetails = require("./routes/aliceDetails");
+const addTrans = require("./routes/addTrans");
+const allDetails = require("./routes/allTrans");
+
 connection();
 app.use(express.json());
 app.use(cors());
@@ -16,4 +19,4 @@ app.use("/api/alice-details", aliceDetails);
 app.use("/api/alice", addTrans);
 app.use("/api", allDetails);
 
-app.listen(port, console.log(`Listening on port ${port}...`));
+app.listen(port, () => console.log(`Listening on port ${port}...`));
